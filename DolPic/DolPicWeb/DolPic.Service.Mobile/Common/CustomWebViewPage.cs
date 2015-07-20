83329129317bfb502da564eca33ce6eded5564b3
chcjswo <chcjswo@gmail.com>
2015-07-20 15:27:32 +0900

    모바일 페이지 수정
    모바일 사이트 모바일 전용 페이지로 수정

---
+++ DolPic/DolPicWeb/DolPic.Service.Mobile/Common/CustomWebViewPage.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace DolPic.Service.Mobile.Common
+{
+    public abstract class CustomWebViewPage<T> : WebViewPage<T>
+    {
+
+        protected CustomWebViewPage()
+        {
+        }
+    }
+}
