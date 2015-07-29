b1ada83f50343583975ec7c84f2d47400a2d93d0
chcjswo <chcjswo@gmail.com>
2015-07-30 01:22:42 +0900

    리팩토리중

---
+++ DolPic/DolPicCrawler/DolPicCrawler/Image/ImageService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DolPicCrawler.Image
+{
+    public class ImageService
+    {
+        private static readonly ImageService _imageService;
+
+        /// <summary>
+        /// static 초기화
+        /// </summary>
+        static ImageService()
+        {
+            _imageService = new ImageService();
+        }
+
+        public static ImageService getInstance
+        {
+            get
+            {
+                return _imageService;
+            }
+        }
+
+        private ImageService() { }
+    }
+}
