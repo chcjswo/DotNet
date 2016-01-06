a4b0c8d671582ebf8675c3f369faf24d3f1e990f
chcjswo <chcjswo@gmail.com>
2016-01-06 18:41:47 +0900

    점심 뭐 먹지 작업중
    점심 뭐 먹지 페이지 추가

---
+++ DolPic/DolPicWeb/DolPic.Service.Mobile/Controllers/LunchController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DolPic.Service.Mobile.Controllers
+{
+    public class LunchController : Controller
+    {
+        //
+        // GET: /Lunch/
+
+        public ActionResult Index()
+        {
+            return RedirectToAction("WhatEat");
+        }
+
+        public ActionResult WhatEat()
+        {
+            return View();
+        }
+
+        public ActionResult Pay()
+        {
+            return View();
+        }
+
+        public ActionResult Share()
+        {
+            return View();
+        }
+
+    }
+}
